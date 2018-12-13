3bb3d7456abde2310f2710a6bc4b759f7ffae31f
Tomasz Barański <tbaranski@ad.pgd>
2018-12-13 15:08:52 +0100

    Initial commit.

---
+++ bin/lincor.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+const lincor = require('lincor')
+const fs = require('fs')
+const program = require('commander')
+
+function signIn() {
+  try {
+    const credentialsFile = fs.readFileSync(program.credentials)
+    const credentials = JSON.parse(credentialsFile.toString())
+    lincor.init(credentials)
+  } catch (e) {
+    lincor.init()
+  }
+}
+
+function printVersion() {
+  lincor.init()
+  lincor.getVersion().then(version => console.log(version))
+}
+
+function downloadObjects(cmd) {
+  signIn()
+  lincor.getCars().then(cars => {
+    let json = JSON.stringify(cars)
+    if (cmd.stdOut) {
+      console.log(json)
+    } else {
+      fs.writeFileSync(cmd.file, json)
+    }
+  })
+}
+
+program.description('CLI interface to Lincor API.')
+
+program.option(
+  '-c, --credentials [path]',
+  'Credentials file path.',
+  'credentials.json'
+)
+
+program
+  .command('version')
+  .alias('v')
+  .description('Get Lincor API version number.')
+  .action(printVersion)
+
+program
+  .command('download-objects')
+  .alias('d')
+  .description('Download all available objects.')
+  .option('-f, --file [file name]', 'Output file name.', 'objects.json')
+  .option(
+    '-s, --std-out',
+    'Print results to standard output instead of writing to file.'
+  )
+  .action(downloadObjects)
+
+program.parse(process.argv)
