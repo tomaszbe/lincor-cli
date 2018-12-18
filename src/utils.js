5327895253b7451bc53823c123f756d0c4ace57a
Tomasz Barański <tomaszbe@hotmail.com>
2018-12-18 13:26:56 +0100

    Fix credentials file path resolving.

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,16 @@
 const fs = require('fs')
 const lincor = require('lincor')
+const path = require('path')
 
 const utils = {}
 
-utils.signIn = function () {
+utils.signIn = function() {
   try {
-    const credentialsFile = fs.readFileSync(program.credentials)
+    let credentialsFilePath = program.credentials
+    if (!path.isAbsolute(credentialsFilePath)) {
+      credentialsFilePath = path.join(process.cwd(), credentialsFilePath)
+    }
+    const credentialsFile = fs.readFileSync(credentialsFilePath)
     const credentials = JSON.parse(credentialsFile.toString())
     lincor.init(credentials)
   } catch (e) {
